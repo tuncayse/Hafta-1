5d5530f54616cfb0d2a7509db7bb03b8c8773164
tuncayse <143759353+tuncayse@users.noreply.github.com>
2023-09-15 17:52:55 +0300

    Çin Zodyağı

---
+++ ÇinZodyağı/src/com/company/Main.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int dogumYili,kalansayi;
+
+        Scanner inp = new Scanner(System.in);
+
+        //kullanıcıdan doğum yılı istenir.
+        System.out.print("Doğum Yılınız : ");
+        dogumYili = inp.nextInt();
+
+        kalansayi = dogumYili % 12;
+
+        switch (kalansayi)
+        {
+            case 0:
+                System.out.println("Çin Zodyağı Burcunuz : Maymun");
+                break;
+            case 1:
+                System.out.println("Çin Zodyağı Burcunuz : Horoz");
+                break;
+            case 2:
+                System.out.println("Çin Zodyağı Burcunuz : Köpek");
+                break;
+            case 3:
+                System.out.println("Çin Zodyağı Burcunuz : Domuz");
+                break;
+            case 4:
+                System.out.println("Çin Zodyağı Burcunuz : Fare");
+                break;
+            case 5:
+                System.out.println("Çin Zodyağı Burcunuz : Öküz");
+                break;
+            case 6:
+                System.out.println("Çin Zodyağı Burcunuz : Kaplan");
+                break;
+            case 7:
+                System.out.println("Çin Zodyağı Burcunuz : Tavşan");
+                break;
+            case 8:
+                System.out.println("Çin Zodyağı Burcunuz : Ejderha");
+                break;
+            case 9:
+                System.out.println("Çin Zodyağı Burcunuz : Yılan");
+                break;
+            case 10:
+                System.out.println("Çin Zodyağı Burcunuz : At");
+                break;
+            case 11:
+                System.out.println("Çin Zodyağı Burcunuz : Koyun");
+                break;
+        }
+
+
+    }
+}
